36a31992417e6040642835d25b02fd266412798a
rainty02 <rainty02@gmail.com>
2021-06-23 11:01:20 +0900

    DB 수정

--- DB 소스.sql
+++ DB 소스.sql
@@ -1,33 +1,38 @@
+-- member 테이블
 create table member (
 memcode integer constraint mem_PK primary key,
-name varchar2(50) not null,
+name varchar2(50) unique not null,
 id varchar2(50) unique not null,
 pw varchar2(50) not null,
 address varchar2(255),
 phone varchar2(30) not null,
 point number(10) default '0' not null
 );
-
+-- sale 테이블
 create table sale (
 salecode integer constraint sale_PK primary key,
 sname varchar2(50) not null,
 price integer not null,
 saledate date default sysdate,
-memcode integer constraint sale_FK references member(memcode)
+id varchar2(50) constraint sale_FK references member(id)
 );
-
-desc member;
-select * from member order by memcode;
-select * from member;
-insert into member (memcode, name, id, pw, address, phone) values (member_sq.nextval, 'name', 'f', '123', '서울', '02');
-
-commit;
-select * from member where id = 'a';
+-- member 시퀀스
 create sequence member_sq
 start with 1;
 
-drop table member;
-drop table sale;
-insert into member values(member_sq.nextval, '?', '?', '?', '?', '?');
-update member set name = ?, pw =?, address = ?, phone = ? where =
-select id from member where = 
+-- member 전체
+select * from member;
+
+-- member 값
+-- id는 a,b,c순
+-- 비밀번호는 1234 통일
+insert into member (memcode, name, id, pw, address, phone) values (member_sq.nextval, '김은선', 'a', '1234', '서울', '01012345678');
+insert into member (memcode, name, id, pw, address, phone) values (member_sq.nextval, '위세라', 'b', '1234', '서울', '01012345678');
+insert into member (memcode, name, id, pw, address, phone) values (member_sq.nextval, '이윤성', 'c', '1234', '서울', '01012345678');
+insert into member (memcode, name, id, pw, address, phone) values (member_sq.nextval, '이현우', 'd', '1234', '서울', '01012345678');
+insert into member (memcode, name, id, pw, address, phone) values (member_sq.nextval, '박지성', 'e', '1234', '서울', '01012345678');
+insert into member (memcode, name, id, pw, address, phone) values (member_sq.nextval, '이강인', 'f', '1234', '서울', '01012345678');
+
+-- 커밋
+commit;
+
